681b0bad4d0c43b425c5896b1599d0b11677fbca
c004245 <c004245@naver.com>
2023-10-15 23:33:55 +0900

    chore: onboarding ui notosans

--- app/src/main/java/com/example/compose_pet_journal/ui/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/compose_pet_journal/ui/onboarding/OnBoardingScreen.kt
@@ -1,6 +1,7 @@
 package com.example.compose_pet_journal.ui.onboarding
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +17,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.compose_pet_journal.ui.theme.black21
+import com.example.compose_pet_journal.ui.theme.notosanskr
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,38 +34,48 @@ fun OnBoardingScreen() {
                 .padding(paddingValues)
                 .fillMaxSize()
         ) {
-
             Column(
-                modifier = Modifier.padding(horizontal = 16.dp),
+                Modifier.background(Color.White),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
                     text = "반려견의 성장을 기록해요",
-                    style = MaterialTheme.
+                    fontFamily = notosanskr,
+                    fontWeight = FontWeight.Bold,
+                    color = black21,
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = "산책, 급여, 배변 등 반려견의 성장을 기록해요.",
+                    fontFamily = notosanskr,
+                    fontWeight = FontWeight.W300,
+                    color = black21,
+                    fontSize = 14.sp,
+                            textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(top = 8.dp)
+
                 )
 
-//                Button(
-//                    onClick = {
-//
-//                    },
-//                    modifier = Modifier
-//                        .animateContentSize()
-//                        .align(Alignment.)
-//                        .padding(bottom = 32.dp)
-//                        .height(56.dp)
-//                        .clip(CircleShape)
-//                ) {
-//                    Text(
-//                        text = "카카오로 시작하기",
-//                        modifier = Modifier.padding(vertical =16.dp),
-//                        color = MaterialTheme.colorScheme.onPrimary
-//                    )
-//                }
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .animateContentSize()
+                        .align(Alignment.CenterHorizontally)
+                        .padding(bottom = 32.dp)
+                        .height(56.dp)
+                        .clip(CircleShape)
+                ) {
+                    Text(
+                        text = "카카오로 시작하기",
+                        modifier = Modifier.padding(vertical =16.dp),
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
             }
-
         }
-
-
     }
 
 }
--- app/src/main/java/com/example/compose_pet_journal/ui/theme/Type.kt
+++ app/src/main/java/com/example/compose_pet_journal/ui/theme/Type.kt
@@ -2,12 +2,22 @@ package com.example.compose_pet_journal.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.compose_pet_journal.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
+
+val notosanskr = FontFamily(
+    Font(R.font.notosanskr_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.notosanskr_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.notosanskr_regular, FontWeight.Normal, FontStyle.Normal)
+)
+
+val Typography = androidx.compose.material3.Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
